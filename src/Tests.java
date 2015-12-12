d682c3e4be6be795f522d8936831a35a8293f191
Ahmed Charfi <hamdouchcharfi@yahoo.fr>
2015-12-12 19:44:47 +0100

    first commit

---
+++ src/Tests.java
@@ -0,0 +1,29 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class Tests {
+	Taschenrechner tr = new Taschenrechner();
+	int a = 4;
+	int b= 5;
+	@Test
+	public void test1()
+	{
+		assertEquals(tr.add(a,b),9);
+		assertEquals(tr.mul(a,b),20);
+	
+	}
+	@Test
+	public void test2()
+	{
+			
+		assertEquals(tr.div(a,b),0);
+		assertEquals(tr.sub(a,b),11);
+		
+		
+	}
+	
+	
+}
